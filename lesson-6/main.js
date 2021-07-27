98941d50191ac43d54790d25a89a807cee157d0c
GladkaV <vktr.gldk10.98@gmail.com>
2021-07-27 21:04:25 +0300

    added hw-6 (additional, classwork)

--- lesson-6/main.js
+++ lesson-6/main.js
@@ -1,8 +1,12 @@
 'use strict';
 
+// HOMEWORK
+
+// Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті
+// https://jsonplaceholder.typicode.com/posts
 fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
-    .then(function (value) {
+    .then((value) => {
         let listUl = document.querySelector('.list_1');
 
         value.forEach(item => {
@@ -16,21 +20,124 @@ fetch('https://jsonplaceholder.typicode.com/posts')
         });
     })
 
-
+// Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті
+// https://jsonplaceholder.typicode.com/comments
 fetch('https://jsonplaceholder.typicode.com/comments')
     .then(response => response.json())
-    .then(function (value) {
-        console.log(value);
+    .then((value) => {
         let listUl = document.querySelector('.list_2');
 
         value.forEach(item => {
             let li = document.createElement('li');
 
             if (item.postId < 4) {
-                console.log(item);
                 li.innerHTML = `name - ${item.name} <br/>
                     email - ${item.email}`;
                 listUl.append(li);
             }
         });
     })
+
+
+// CLASSWORK
+
+// Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті
+// https://jsonplaceholder.typicode.com/posts
+// зробити кнопку до кожного поста.при кліку на яку виводяться в окремий блок всі коментарі поточного поста
+fetch('https://jsonplaceholder.typicode.com/posts/')
+    .then(response => response.json())
+    .then((value) => {
+        let listUl = document.querySelector('.list_3');
+
+        value.forEach(item => {
+            let li = document.createElement('li');
+
+            if (item.id < 10) {
+                li.innerText = `title - ${item.title}`;
+                listUl.append(li);
+
+                let button = document.createElement('button');
+                button.innerText = 'show posts';
+                button.onclick = function () {
+                    fetch(`https://jsonplaceholder.typicode.com/posts/${item.id}/comments/`)
+                        .then(response => response.json())
+                        .then(value => {
+                            let listUlComments = document.querySelector('.list_3-comments');
+                            listUlComments.innerHTML = '';
+
+                            value.forEach(item => {
+                                let li = document.createElement('li');
+                                li.innerText = `${item.name}`;
+                                listUlComments.append(li);
+                            })
+                        })
+                }
+                li.append(button);
+            }
+        });
+    })
+
+// ADDITIONAL 
+
+// Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті
+// https://jsonplaceholder.typicode.com/users
+// кожному елементу юзера створити кнопку, при клику на яку в окремий блок виводяться всі пости поточного юзера.
+// Кожному елементу post створити кнопку, при клику на яку в окремий блок виводяться всі коментарі поточного поста
+let listUl = document.querySelector('.list_4');
+let listUlPosts = document.querySelector('.list_4-posts');
+let listUlComments = document.querySelector('.list_4-comments');
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(value => {
+        value.forEach(item => {
+            let li = document.createElement('li');
+            li.innerText = `name - ${item.name}`;
+            listUl.append(li);
+
+            let button = document.createElement('button');
+            button.innerText = `show posts`;
+            button.onclick = () => {
+                getPosts(item.id)
+            };
+            li.append(button);
+        })
+    })
+
+function getPosts(id) {
+    listUlPosts.innerHTML = '';
+    listUlComments.innerHTML = '';
+
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}/posts`)
+        .then(response => response.json())
+        .then(value => {
+            value.forEach(item => {
+                let li = document.createElement('li');
+                li.innerHTML = `userId - ${item.userId} <br/>
+                                title - ${item.title}`;
+                listUlPosts.append(li);
+
+                let button = document.createElement('button');
+                button.innerText = `show coments`;
+                button.onclick = () => {
+                    getComments(item.id)
+                };
+                li.append(button);
+            })
+        })
+}
+
+function getComments(id) {
+    listUlComments.innerHTML = '';
+    
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+        .then(response => response.json())
+        .then(value => {
+            value.forEach(item => {
+                let li = document.createElement('li');
+                li.innerHTML = `postId - ${item.postId} <br/>
+                                name - ${item.name}`;
+                listUlComments.append(li);
+            })
+        })
+}
